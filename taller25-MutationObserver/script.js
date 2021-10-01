52dd1d98f3a70e219951af2df74f76aa4be2a425
cpcampos <80595340+cpcampos@users.noreply.github.com>
2021-10-01 09:36:55 -0600

    Add files via upload

---
+++ taller25-MutationObserver/script.js
@@ -0,0 +1,32 @@
+const input = document.getElementById('fruit');
+const button = document.getElementById('submit');
+const targetNode = document.getElementById('myList');
+
+button.addEventListener('click', add);
+
+function add() {
+  const li = document.createElement('li');
+  const text = input.value;
+  li.appendChild(document.createTextNode(text));
+  targetNode.appendChild(li);
+}
+
+// Mutation Observer
+
+const config = {
+  attributes: true,
+  childList: true,
+  subtree: true,
+}
+
+const observer = new MutationObserver(function(mutationList, observer){
+  for(const mutation of mutationList) {
+    if(mutation.type === 'childList') {
+      console.log('Child added or removed');
+    } else if (mutation.type === 'attributes') {
+      console.log(`The ${mutation.attributeName} attribute was modified.`);
+    }
+  }
+});
+
+observer.observe(targetNode, config);
